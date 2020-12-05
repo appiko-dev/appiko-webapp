d68748b9052f4885f7f8484dbf4eb2692bdc3488
Alexander Lapik <Alapik101@hotmail.com>
2020-12-05 00:27:09 -0500

    innit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for
+import secrets
+
+app = Flask(__name__, static_url_path="/static")
+
+app.config["SECRET KEY"] = secrets.token_hex(16)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", title="Home")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
