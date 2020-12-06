eab8d94c9bc5b0e66edc5daa6b640bd551f2c397
Alexander Lapik <Alapik101@hotmail.com>
2020-12-05 20:32:33 -0500

    implemented bootstrap

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ app.config["SECRET KEY"] = secrets.token_hex(16)
 
 @app.route("/")
 def home():
-    return render_template("home.html", title="Home")
+    return render_template("home.html", title="home", posts=posts)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
