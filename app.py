037c1375d4100c4156d380138a133711ee14fa5b
Alexander Lapik <Alapik101@hotmail.com>
2020-12-10 16:38:07 -0500

    bootstrap
    site more astheticaly pleasing through bootstrap

--- app.py
+++ app.py
@@ -8,7 +8,22 @@ app.config["SECRET KEY"] = secrets.token_hex(16)
 
 @app.route("/")
 def home():
-    return render_template("home.html", title="home", posts=posts)
+    return render_template("home.html", title="home")
+
+
+@app.route("/products")
+def products():
+    return render_template("products.html", title="products")
+
+
+@app.route("/services")
+def services():
+    return render_template("services.html", title="services")
+
+
+@app.route("/arcade")
+def arcade():
+    return render_template("arcade.html", title="arcade")
 
 
 if __name__ == "__main__":
