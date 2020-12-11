fa9038ec76fb7ae0364d018c2953e8140ca81c71
Alexander Lapik <Alapik101@hotmail.com>
2020-12-10 20:15:03 -0500

    initiated registration page

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 import secrets
 
 app = Flask(__name__, static_url_path="/static")
 
-app.config["SECRET KEY"] = secrets.token_hex(16)
+app.config["SECRET_KEY"] = "491b8c0c72d4fdac6a8a2f3509dc414b"
 
 
 @app.route("/")
@@ -24,6 +25,23 @@ def services():
 @app.route("/arcade")
 def arcade():
     return render_template("arcade.html", title="arcade")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+
+    return render_template("register.html", title="register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="login", form=form)
 
 
 if __name__ == "__main__":
