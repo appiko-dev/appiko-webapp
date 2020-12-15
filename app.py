724233b7e21ef044695f4b6977ea7c64325397d5
Alexander Lapik <Alapik101@hotmail.com>
2020-12-14 21:49:58 -0500

    database implementation

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 import secrets
 
 app = Flask(__name__, static_url_path="/static")
 
 app.config["SECRET_KEY"] = "491b8c0c72d4fdac6a8a2f3509dc414b"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # unique id for our user
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), unique=False,
+                           nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 @app.route("/")
@@ -38,9 +55,18 @@ def register():
     return render_template("register.html", title="register", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "P@$$w0rd":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "danger")
+            return redirect(url_for("home"))
+
     return render_template("login.html", title="login", form=form)
 
 
